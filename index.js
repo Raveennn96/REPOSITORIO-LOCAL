ef881863f8726d00469f3ad63c8bcba1d62aeb3f
Raveennn96 <enzo.rabalo@gmail.com>
2023-09-05 23:56:28 -0300

    trabajofinal

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
+const form = document.getElementById('formulario-contacto');
+const popup = document.getElementById('popup');
+const closeBut = document.getElementById('close-button');
 
-navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("nav-menu_visible");
-
-  if (navMenu.classList.contains("nav-menu_visible")) {
-    navToggle.setAttribute("aria-label", "Cerrar menú");
-  } else {
-    navToggle.setAttribute("aria-label", "Abrir menú");
-  }
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    popup.style.display = 'flex';
+    })
+closeBut.addEventListener('click', function(){
+    popup.style.display = 'none';
 });
+
